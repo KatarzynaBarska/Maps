25a0d36512371e259b01b1caf1b48f9da0b74b51
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-09-06 12:15:48 +0200

    Added possibility to add new ads into database.

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -2,31 +2,34 @@ import {AdEntity, NewAdEntity, SimpleAdEntity} from "../types";
 import {ValidationError} from "../utils/errors";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
+import {v4 as uuid} from 'uuid';
 
 type AdRecordResults = [AdEntity[], FieldPacket[]];
 
 export class AdRecord implements AdEntity {
-   public description: string;
+
+    public description: string;
     public id: string;
     public lat: number;
     public lon: number;
     public name: string;
     public price: number;
     public url: string;
-    constructor(obj: AdEntity) {
+
+    constructor(obj: NewAdEntity) {
         if (!obj.name || obj.name.length > 100) {
             throw new ValidationError('Nazwa ogłoszenia nie może być pusta ani przekraczać 100 znaków.')
         }
-         if(obj.description.length > 1000) {
-             throw new ValidationError('Treść ogłoszenia nie może przekraczać 1000 znaków.');
-         }
-         if(obj.price < 0 || obj.price > 9999999) {
-             throw new ValidationError('Cena nie może być mniejsza niż 0 lub większa niż 9999999.');
-         }
+        if (obj.description.length > 1000) {
+            throw new ValidationError('Treść ogłoszenia nie może przekraczać 1000 znaków.');
+        }
+        if (obj.price < 0 || obj.price > 9999999) {
+            throw new ValidationError('Cena nie może być mniejsza niż 0 lub większa niż 9999999.');
+        }
         //@TODO: Check if URL is valid!
-        if(!obj.url || obj.url.length > 100) {
+        if (!obj.url || obj.url.length > 100) {
             throw new ValidationError('Link ogłoszenia nie może być pusty, ani przekraczać 100 znaków');
-    }
+        }
         if (typeof obj.lat !== 'number' || typeof obj.lon !== 'number') {
             throw new ValidationError('Nie można zlokalizować ogłoszenia.')
         }
@@ -39,30 +42,40 @@ export class AdRecord implements AdEntity {
         this.lon = obj.lon;
         this.url = obj.url;
     }
+
     static async getOne(id: string): Promise<AdRecord | null> {
         const [results] = await pool.execute("SELECT * FROM `ads` WHERE  `id` = :id", {
             id,
-        })as AdRecordResults;
+        }) as AdRecordResults;
 
         return results.length === 0 ? null : new AdRecord(results[0]);
 
     }
 
     static async findAll(name: string): Promise<SimpleAdEntity[]> {
-       const [results] = await pool.execute("SELECT * FROM `ads` WHERE `name` LIKE :search", {
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE `name` LIKE :search", {
             search: `%${name}%`,
         }) as AdRecordResults;
 //tworzymy obj na bazie obj result(AdEntity) i w ten sposób powstaje proste filtrowanie informacji przesyłanych na frontend podczas wyszukiwania metoda .findAll
-       return results.map(result => {
-           const {
-               id, lat, lon,
-           } = result;
-           return {
-               id, lat, lon,
-           };
-       });
+        return results.map(result => {
+            const {
+                id, lat, lon,
+            } = result;
+            return {
+                id, lat, lon,
+            };
+        });
     }
 
+    async insert(): Promise<void> {
+        if (!this.id) {
+            this.id = uuid();
+        } else {
+            throw new Error('Cannot insert something that is already inserted.');
+        }
+        await pool.execute("INSERT INTO `ads` (`id`, `name`, `description`, `url`, `lat`, `lon`) VALUES (:id, :name, :description, :url, :lat, :lon)", this);
+    }
+}
 
     // async insert(): Promise<string> {
     //     if (!this.id) {
@@ -77,4 +90,3 @@ export class AdRecord implements AdEntity {
     //     return this.id;
     // }
 
-}
--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -2,9 +2,19 @@ import {AdRecord} from "../records/ad.record";
 import {pool} from "../utils/db";
 import {AdEntity} from "../types";
 
+const defaultObj = {
+    description: "Test descr",
+        lat: 3,
+        lon: 2,
+        name: "test name",
+        price: 1,
+        url: "https://localhost:8080",
+};
+
 afterAll(async () => {
     await pool.end();
 });
+
 test('AdRecord returns data from database for one entry.', async () => {
 
     const ad = await  AdRecord.getOne('abc');
@@ -53,4 +63,25 @@ test('AdRecord.findAll returns smaller amount of data.', async () => {
    expect((ads[0] as AdEntity).url).toBeUndefined();
 
 
+});
+
+test('AdRecord.insert inserts new UUID.', async () => {
+
+    const ad = new AdRecord( defaultObj);
+await ad.insert();
+
+expect(ad.id).toBeDefined();
+expect(typeof ad.id).toBe('string');
+});
+
+test('AdRecord.insert inserts data to database.', async () => {
+
+    const ad = new AdRecord( defaultObj);
+    await ad.insert();
+
+    const foundAd = await AdRecord.getOne(ad.id);
+
+    expect(foundAd).toBeDefined();
+    expect(foundAd).not.toBeNull();
+    expect(foundAd.id).toBe(ad.id);
 });
